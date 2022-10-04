636c5d453255bc534fab556f308c37ab705dac9e
chris <christhirst@gmail.com>
2022-10-04 14:44:43 +0200

    testing

--- cmd/init.go
+++ cmd/init.go
@@ -1,19 +1,16 @@
 package cmd
 
 import (
-	"errors"
+	"encoding/hex"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"net/http/httptest"
 	"os"
 
-	"github.com/gorilla/sessions"
-	"github.com/jcmturner/goidentity/v6"
 	"github.com/jcmturner/gokrb5/v8/client"
 	"github.com/jcmturner/gokrb5/v8/config"
 	"github.com/jcmturner/gokrb5/v8/keytab"
-	"github.com/jcmturner/gokrb5/v8/service"
 	"github.com/jcmturner/gokrb5/v8/spnego"
 )
 
@@ -40,109 +37,53 @@ const (
 )
 
 func Run() {
-	//l := log.New(os.Stderr, "GOKRB5 Client: ", log.LstdFlags)
+	l := log.New(os.Stderr, "GOKRB5 Client: ", log.LstdFlags)
 
-	//defer profile.Start(profile.TraceProfile).Stop()
+	// defer profile.Start(profile.TraceProfile).Stop()
 	// Load the keytab
-
-	kt, err := keytab.Load("./krb5.keytab")
-	fmt.Println(err)
-	s := httpServer()
-	defer s.Close()
-	c, _ := config.NewFromString(kRB5CONF)
-	c.LibDefaults.NoAddresses = true
-	cl := client.NewWithKeytab("host/client1.ahmad.io", "AHMAD.IO", kt, c)
-	httpRequest(s.URL, cl)
-	/* th := http.HandlerFunc(testAppHandler)
-
-	// Set up handler mappings wrapping in the SPNEGOKRB5Authenticate handler wrapper
-	mux := http.NewServeMux()
-	mux.Handle("/", spnego.SPNEGOKRB5Authenticate(th, kt, service.Logger(l), service.SessionManager(NewSessionMgr("gokrb5"))))
-
-	// Start up the web server
-	log.Fatal(http.ListenAndServe(port, mux)) */
-}
-
-func httpRequest(url string, cl *client.Client) {
-	l := log.New(os.Stderr, "GOKRB5 Client: ", log.Ldate|log.Ltime|log.Lshortfile)
-
-	err := cl.Login()
+	kb, _ := hex.DecodeString(testdata.TESTUSER2_KEYTAB)
+	kt := keytab.New()
+	err := kt.Unmarshal(kb)
 	if err != nil {
-		l.Printf("Error on AS_REQ: %v\n", err)
+		l.Fatalf("could not load client keytab: %v", err)
 	}
-	r, _ := http.NewRequest("GET", url, nil)
-	err = spnego.SetSPNEGOHeader(cl, r, "HTTP/client1.ahmad.io")
+
+	// Load the client krb5 config
+	conf, err := config.NewConfigFromString(kRB5CONF)
 	if err != nil {
-		l.Printf("Error setting client SPNEGO header: %v", err)
+		l.Fatalf("could not load krb5.conf: %v", err)
 	}
-	/*
-		httpResp, err := http.DefaultClient.Do(r)
-		if err != nil {
-			l.Printf("Request error: %v\n", err)
-		}
-		fmt.Fprintf(os.Stdout, "Response Code: %v\n", httpResp.StatusCode)
-		content, _ := io.ReadAll(httpResp.Body)
-		fmt.Fprintf(os.Stdout, "Response Body:\n%s\n", content) */
-}
-
-func httpServer() *httptest.Server {
-	l := log.New(os.Stderr, "GOKRB5 Service Tests: ", log.Ldate|log.Ltime|log.Lshortfile)
-	kt, err := keytab.Load("./krb5.keytab")
-	fmt.Println(err)
-	th := http.HandlerFunc(testAppHandler)
-	s := httptest.NewServer(spnego.SPNEGOKRB5Authenticate(th, kt, service.Logger(l)))
-	return s
-}
-
-func testAppHandler(w http.ResponseWriter, r *http.Request) {
-	creds := goidentity.FromHTTPRequestContext(r)
-	fmt.Fprint(w, "<html>\n<p><h1>TEST.GOKRB5 Handler</h1></p>\n")
-	if creds != nil && creds.Authenticated() {
-		fmt.Fprintf(w, "<ul><li>Authenticed user: %s</li>\n", creds.UserName())
-		fmt.Fprintf(w, "<li>User's realm: %s</li></ul>\n", creds.Domain())
-	} else {
-		w.WriteHeader(http.StatusUnauthorized)
-		fmt.Fprint(w, "Authentication failed")
+	addr := os.Getenv("TEST_KDC_ADDR")
+	if addr != "" {
+		conf.Realms[0].KDC = []string{addr + ":88"}
 	}
-	fmt.Fprint(w, "</html>")
-	return
-}
 
-type SessionMgr struct {
-	skey       []byte
-	store      sessions.Store
-	cookieName string
-}
+	// Create the client with the keytab
+	cl := client.NewClientWithKeytab("testuser2", "TEST.GOKRB5", kt, conf, client.Logger(l), client.DisablePAFXFAST(true))
 
-func NewSessionMgr(cookieName string) SessionMgr {
-	skey := []byte("thisistestsecret") // Best practice is to load this key from a secure location.
-	return SessionMgr{
-		skey:       skey,
-		store:      sessions.NewCookieStore(skey),
-		cookieName: cookieName,
-	}
-}
-
-func (smgr SessionMgr) Get(r *http.Request, k string) ([]byte, error) {
-	s, err := smgr.store.Get(r, smgr.cookieName)
+	// Log in the client
+	err = cl.Login()
 	if err != nil {
-		return nil, err
+		l.Fatalf("could not login client: %v", err)
 	}
-	if s == nil {
-		return nil, errors.New("nil session")
-	}
-	b, ok := s.Values[k].([]byte)
-	if !ok {
-		return nil, fmt.Errorf("could not get bytes held in session at %s", k)
-	}
-	return b, nil
-}
 
-func (smgr SessionMgr) New(w http.ResponseWriter, r *http.Request, k string, v []byte) error {
-	s, err := smgr.store.New(r, smgr.cookieName)
+	// Form the request
+	url := "http://localhost" + port
+	r, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return fmt.Errorf("could not get new session from session manager: %v", err)
+		l.Fatalf("could create request: %v", err)
 	}
-	s.Values[k] = v
-	return s.Save(r, w)
+
+	spnegoCl := spnego.NewClient(cl, nil, "HTTP/host.test.gokrb5")
+
+	// Make the request
+	resp, err := spnegoCl.Do(r)
+	if err != nil {
+		l.Fatalf("error making request: %v", err)
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		l.Fatalf("error reading response body: %v", err)
+	}
+	fmt.Println(string(b))
 }
